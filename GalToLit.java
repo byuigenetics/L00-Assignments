0467a4432456eff8ddab0ad7d7a6dd1430d48172
byuigenetics <way12003@byui.edu>
2014-04-24 17:41:40 -0600

    Create GalToLit.java
    this is the 1st file

---
+++ GalToLit.java
@@ -0,0 +1,14 @@
+public class GalToLit {
+
+		public static void main(String args[]) {
+
+			double gallons; // number of gallons
+			double liters; // conversion from gallons to liters
+
+	gallons = 10;
+	liters = gallons * 3.7854; // convert to liters
+	
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+		
+	}
+}
